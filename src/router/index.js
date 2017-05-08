1e5f89a61255b9f5988a603c9ddeefefa734049d
Still_Learning <smslam2999@gmail.com>
2017-05-08 18:15:58 +0600

    gittu loose

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import GittuComponents from '@/components/git-component'
+import GitComponents from '@/components/git-component'
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: GittuComponents
+      component: GitComponents
     }
   ]
 })
