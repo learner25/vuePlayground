f74e5dfd64b84a210a081ce0e5095666c541c909
Still_Learning <smslam2999@gmail.com>
2017-05-08 13:54:50 +0600

    added vue component git-component from branch dev-saimon

---
+++ src/components/git-component.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div>
+{{message}}
+</div>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return{
+     message:"this is new message"
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
